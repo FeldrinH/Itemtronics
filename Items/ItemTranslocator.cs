7b4d4576f8269c1b93bdf27bcc6193097cb15810
FeldrinH <fhandswitch@gmail.com>
2016-09-07 18:19:38 +0300

    Added Item Translocator

---
+++ Items/ItemTranslocator.cs
@@ -0,0 +1,38 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Itemtronics.Items
+{
+	public class ItemTranslocator : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Item Translocator";
+			item.width = 10;
+			item.height = 12;
+			item.maxStack = 99;
+			AddTooltip("Moves items from one adjacent chest to another when activated");
+			AddTooltip2("Right click to change direction");
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.mech = true;
+			item.value = 1000;
+			item.createTile = mod.TileType("ItemTranslocator");
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BoosterTrack, 1);
+			recipe.AddIngredient(ItemID.Wire, 5);
+			recipe.AddIngredient(ItemID.Actuator, 5);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Tiles/ItemTranslocator.cs
@@ -0,0 +1,41 @@
+﻿using Itemtronics.Util;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Itemtronics.Tiles
+{
+	class ItemTranslocator : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileFrameImportant[Type] = true;
+			Main.tileLavaDeath[Type] = false;
+			Main.tileValue[Type] = 1000;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.None, 0, 0);
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.addTile(Type);
+			drop = mod.ItemType("ItemTranslocator");
+		}
+
+		/*public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
+		{
+			Tile tile = Main.tile[i, j];
+			tile.frameX = 36;
+			//tile.frameY = 0;
+			return true;
+		}*/
+
+		public override void RightClick(int i, int j)
+		{
+			Tile tile = Main.tile[i, j];
+			tile.frameX = (short)((tile.frameX + 18) % 72);
+		}
+	}
+}
